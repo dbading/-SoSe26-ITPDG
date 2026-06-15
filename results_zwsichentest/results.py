293783d1bd1638f1b36c5ec65075f50015d61b3d
Daniel Bading <danielbading@MacDani.local>
2026-06-15 17:47:46 +0200

    added candlebar plot and violin plot to see the distribution of points in each question

--- results_zwsichentest/results.py
+++ results_zwsichentest/results.py
@@ -22,44 +22,44 @@ df.rename(columns={
     'F 9 /4,00': 'F9', 
     }, inplace=True)
 
-# give me all keys in the dataframe
-# print(df.keys())
-
-# check for missing values in the dataframe
-# print(df.isnull().sum())
-
-# print list of values to 'Total' column
-# print(df['Total'].tolist())
-
-# # # convert the string in the dataframe to a float, replace the comma with a dot
-# df['Total'] = df['Total'].str.replace(',', '.').astype(float)
-# df['F1'] = df['F1'].str.replace(',', '.').astype(float)
-# df['F2'] = df['F2'].str.replace(',', '.').astype(float)
-# df['F3'] = df['F3'].str.replace('-', '0.0').astype(str)
-# df['F3'] = df['F3'].str.replace(',', '.').astype(float)
-# df['F4'] = df['F4'].str.replace(',', '.').astype(float)
-# df['F5'] = df['F5'].str.replace('-', '0.0').astype(str)
-# df['F5'] = df['F5'].str.replace(',', '.').astype(float)
-# df['F6'] = df['F6'].str.replace('-', '0.0').astype(str)
-# df['F6'] = df['F6'].str.replace(',', '.').astype(float)
-# df['F7'] = df['F7'].str.replace('-', '0.0').astype(str)
-# df['F7'] = df['F7'].str.replace(',', '.').astype(float)
-# df['F8'] = df['F8'].str.replace('-', '0.0').astype(str)
-# df['F8'] = df['F8'].str.replace(',', '.').astype(float)
-# df['F9'] = df['F9'].str.replace('-', '0.0').astype(str)
-# df['F9'] = df['F9'].str.replace(',', '.').astype(float)
-
 for i in range(1,10):
     df[f'F{i}'] = df[f'F{i}'].str.replace('-', '0.0').astype(str)
     df[f'F{i}'] = df[f'F{i}'].str.replace(',', '.').astype(float)
 
 # print(df['F3'].tolist())
 
-# ---- histogram of F1 ---- #
+# # make a barchart for each F1-F9, with how many people got how mony points in each F1-F9, with the x-axis being the points and the y-axis being the frequency of people who got that many points
+# for i in range(1,10):
+#     plt.bar(df[f'F{i}'].value_counts().index, df[f'F{i}'].value_counts().values)
+#     plt.title(f'Bar Chart of F{i}')
+#     plt.xlabel(f'F{i} Score')
+#     plt.ylabel('Frequency')
+#     plt.show()
 
-# print(df['F3'].tolist())
+# --- candle bar plot --- #
+means = []
+stds = []
+for i in range(1,10):
+    means.append(df[f'F{i}'].mean())
+    stds.append(df[f'F{i}'].std())
+plt.errorbar(range(1,10), means, yerr=stds, fmt='o')
+plt.title('Mean and Standard Deviation of F1-F9')
+plt.xlabel('F1-F9')
+plt.ylabel('Mean and Standard Deviation')
+plt.xticks(range(1,10))
+plt.show()  
 
-i = 5
+# --- violin plot --- #
+data = [df[f'F{i}'] for i in range(1,10)]
+plt.violinplot(data, showmeans=True)
+plt.title('Violin Plot of F1-F9')
+plt.xlabel('F1-F9')
+plt.ylabel('Scores')
+plt.xticks(range(1,10))
+plt.show()
+
+# ---- histogram of Fi ---- #
+i = 9
 
 plt.hist(df[f'F{i}'], bins=int(max(df[f'F{i}'])), edgecolor='black')
 plt.title(f'Histogram of F{i}')
@@ -67,12 +67,4 @@ plt.xlabel(f'F{i} Score')
 plt.ylabel('Frequency')
 plt.show()
 
-# distribution of all F1-F9 in one plot
-plt.figure(figsize=(10,6))
-for i in range(1,10): # now we like to have a distribution as plt.plot for each F1-F9 in one plot
-    plt.plot(df[f'F{i}'], label=f'F{i}')
-plt.title('Distribution of F1-F9')
-plt.xlabel('Index')
-plt.ylabel('Score')
-plt.legend()
-plt.show()  
+print(df[f'F{i}'].tolist())
